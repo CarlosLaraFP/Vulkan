4a04a2780389c85eab3dfde920a5d5387a6ae72a
CarlosLaraFP <carloslarafp@gmail.com>
2024-03-12 12:24:21 -0700

    Created VkInstance

--- main.cpp
+++ main.cpp
@@ -1,4 +1,145 @@
 #define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h> // GLFW will include its own definitions and automatically load <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib> // provides the EXIT_SUCCESS and EXIT_FAILURE macros
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+// A lot of information in Vulkan is passed through structs instead of function parameters.
+class HelloTriangleApplication 
+{
+public:
+    void run() 
+    {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window = nullptr;
+    VkInstance instance;
+
+    void initWindow() 
+    {
+        if (!glfwInit())
+        {
+            throw std::runtime_error("GLFW could not be initialized.");
+        }
+
+        // Because GLFW was originally designed to create an OpenGL context, we need to tell it to not create an OpenGL context.
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        // Because handling resized windows takes special care that we’ll look into later, disable it for now.
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        // The 4th parameter allows you to optionally specify a monitor to open the window on and the last parameter is only relevant to OpenGL.
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+
+        if (window == nullptr)
+        {
+            glfwTerminate();
+
+            throw std::runtime_error("Window could not be created.");
+        }
+    }
+
+    /*
+        The instance is the connection between your application and the Vulkan library and 
+        creating it involves specifying some details about your application to the driver.
+    */
+    void createInstance()
+    {
+        /*
+            This data is technically optional, but it may provide some useful information to the driver in order to optimize 
+            your specific application (e.g. because it uses a well-known graphics engine with certain special behavior).
+        */
+        VkApplicationInfo appInfo {};
+
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_3;
+
+        /*
+            This is not optional and tells the Vulkan driver which global extensions and validation layers we want to use. 
+            Global here means that they apply to the entire program and not a specific device.
+        */
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        VkInstanceCreateInfo createInfo {};
+
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        /*
+            Specify the desired global extensions; since Vulkan is a platform agnostic API, we need an extension to interface with the window 
+            system. GLFW has a handy built-in function that returns the extension(s) it needs to do that - which we can pass to the struct.
+        */
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        /*
+            This is the general pattern followed by object creation function parameters in Vulkan:
+
+            Pointer to struct with creation info.
+            Pointer to custom allocator callbacks, always nullptr in this tutorial.
+            Pointer to the variable that stores the handle to the new object.
+        
+            Nearly all Vulkan functions return a value of type VkResult that is either VK_SUCCESS or an error code. 
+            To check if the instance was created successfully, we can just check for the success value:
+        */
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) 
+        {
+            throw std::runtime_error("Failed to create VkInstance.");
+        }
+    }
+
+    void initVulkan() 
+    {
+        createInstance();
+    }
+
+    void mainLoop() 
+    {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() 
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+};
+
+int main() 
+{
+    HelloTriangleApplication app;
+
+    try 
+    {
+        app.run();
+    }
+    catch (const std::exception& e) 
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+/*
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #define GLM_FORCE_RADIANS
@@ -7,6 +148,7 @@
 #include <glm/mat4x4.hpp>
 
 #include <iostream>
+
 
 int main() 
 {
@@ -35,3 +177,4 @@ int main()
 
     return 0;
 }
+*/
