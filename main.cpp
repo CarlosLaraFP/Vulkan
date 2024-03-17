8f5dd17b7e8b10e736473ecceaa79d8a2f383ccd
CarlosLaraFP <carloslarafp@gmail.com>
2024-03-17 08:27:24 -0700

    Created command buffer

--- main.cpp
+++ main.cpp
@@ -244,6 +244,8 @@ private:
     VkPipelineLayout pipelineLayout;
     VkPipeline graphicsPipeline;
     std::vector<VkFramebuffer> swapChainFramebuffers;
+    VkCommandPool commandPool;
+    VkCommandBuffer commandBuffer; // automatically freed when its command pool is destroyed
 
     void initWindow() 
     {
@@ -462,6 +464,35 @@ private:
         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) 
         {
             throw std::runtime_error("Failed to create VkInstance.");
+        }
+    }
+
+    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo)
+    {
+        createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        createInfo.pfnUserCallback = debugCallback; // pointer to the callback function
+        createInfo.pUserData = nullptr; // Optional, such as a pointer to the HelloTriangleApplication class
+    }
+
+    void setupDebugMessenger()
+    {
+        if (!enableValidationLayers) return;
+
+        // Fill in a struct with details about the messenger and its callback.
+        VkDebugUtilsMessengerCreateInfoEXT createInfo;
+
+        populateDebugMessengerCreateInfo(createInfo);
+
+        /*
+            Since the debug messenger is specific to our Vulkan instance and its layers, it needs to be explicitly specified as first argument.
+            You will also see this pattern with other child objects later on.
+        */
+        if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed to set up debug messenger.");
         }
     }
 
@@ -1264,30 +1295,6 @@ private:
             The minDepth and maxDepth values specify the range of depth values for the framebuffer and must be within [0.0f, 1.0f].
         */
         /*
-        VkViewport viewport {};
-
-        viewport.x = 0.0f;
-        viewport.y = 0.0f;
-        viewport.width = (float)swapChainExtent.width;
-        viewport.height = (float)swapChainExtent.height;
-        viewport.minDepth = 0.0f;
-        viewport.maxDepth = 1.0f;
-        */
-
-        /*
-            While viewports define the transformation from the image to the framebuffer, scissor rectangles define in 
-            which regions pixels will actually be stored. Any pixels outside the scissor rectangles will be discarded 
-            by the rasterizer. They function like a filter rather than a transformation. 
-            If we want to draw to the entire framebuffer, specify a scissor rectangle that covers it entirely.
-        */
-        /*
-        VkRect2D scissor{};
-
-        scissor.offset = { 0, 0 };
-        scissor.extent = swapChainExtent;
-        */
-
-        /*
             Viewport(s) and scissor rectangle(s) can either be specified as a static part of the pipeline or as a dynamic
             state set in the command buffer. While the former is more in line with the other states it’s often convenient
             to make viewport and scissor state dynamic as it gives you a lot more flexibility. This is very common and all
@@ -1565,6 +1572,195 @@ private:
         }
     }
 
+    /*
+        Commands in Vulkan, like drawing operations and memory transfers, are not executed directly using function calls. We have to record 
+        all of the operations you want to perform in command buffer objects. The advantage of this is that when we are ready to tell 
+        Vulkan what we want to do, all of the commands are submitted together and Vulkan can more efficiently process the commands since 
+        all of them are available together. In addition, this allows command recording to happen in multiple threads if so desired.
+    */
+    void createCommandPool()
+    {
+        QueueFamilyIndices queueFamilyIndices = findQueueFamilies(physicalDevice);
+
+        VkCommandPoolCreateInfo poolInfo {};
+
+        poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+        /*
+            There are two possible flags for command pools:
+
+            VK_COMMAND_POOL_CREATE_TRANSIENT_BIT: Hint that command buffers are rerecorded with new commands very often 
+            (may change memory allocation behavior).
+
+            VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT: Allow command buffers to be rerecorded individually, 
+            without this flag they all have to be reset together.
+
+            We will be recording a command buffer every frame, so we want to be able to reset and rerecord over it. 
+            Thus, we need to set the VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT flag bit for our command pool.
+        */
+        poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+        /*
+            Command buffers are executed by submitting them on one of the device queues, like the graphics and presentation queues 
+            we retrieved. Each command pool can only allocate command buffers that are submitted on a single type of queue. 
+            We are going to record commands for drawing, which is why we have chosen the graphics queue family.
+        */
+        poolInfo.queueFamilyIndex = queueFamilyIndices.graphicsFamily.value();
+
+        if (vkCreateCommandPool(device, &poolInfo, nullptr, &commandPool) != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed to create command pool.");
+        }
+    }
+
+    void createCommandBuffer()
+    {
+        VkCommandBufferAllocateInfo allocInfo {};
+
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.commandPool = commandPool;
+        /*
+            The level parameter specifies if the allocated command buffers are primary or secondary command buffers.
+
+            VK_COMMAND_BUFFER_LEVEL_PRIMARY: Can be submitted to a queue for execution, but cannot be called from other command buffers.
+
+            VK_COMMAND_BUFFER_LEVEL_SECONDARY: Cannot be submitted directly, but can be called from primary command buffers.
+
+            We won’t make use of the secondary command buffer functionality here, but you can 
+            imagine that it’s helpful to reuse common operations from primary command buffers.
+        */
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandBufferCount = 1; // allocate a single command buffer from the command pool
+
+        if (vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer) != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed to allocate command buffer(s).");
+        }
+    }
+
+    // writes the commands we want to execute into a command buffer
+    void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex)
+    {
+        VkCommandBufferBeginInfo beginInfo {};
+
+        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        /*
+            The flags parameter specifies how we’re going to use the command buffer. The following values are available:
+
+            VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT: The command buffer will be rerecorded right after executing it once.
+
+            VK_COMMAND_BUFFER_USAGE_RENDER_PASS_CONTINUE_BIT: This is a secondary command buffer that will be entirely within a single render pass.
+
+            VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT: The command buffer can be resubmitted while it is also already pending execution.
+
+            None of these flags are applicable for us right now.
+        */
+        beginInfo.flags = 0; // Optional
+        /*
+            The pInheritanceInfo parameter is only relevant for secondary command buffers. 
+            It specifies which state to inherit from the calling primary command buffers.
+        */
+        beginInfo.pInheritanceInfo = nullptr; // Optional
+
+        // If the command buffer was already recorded once, then a call to vkBeginCommandBuffer will implicitly reset it. 
+        // It’s not possible to append commands to a buffer at a later time.
+        if (vkBeginCommandBuffer(commandBuffer, &beginInfo) != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed to begin recording command buffer.");
+        }
+
+        VkRenderPassBeginInfo renderPassInfo {};
+        /*
+            The first parameters are the render pass itself and the attachments to bind. We created a framebuffer for each swap chain image 
+            where it is specified as a color attachment. Thus we need to bind the framebuffer for the swapchain image we want to draw to. 
+            Using the imageIndex parameter which was passed in, we can pick the right framebuffer for the current swapchain image.
+        */
+        renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+        renderPassInfo.renderPass = renderPass;
+        renderPassInfo.framebuffer = swapChainFramebuffers[imageIndex];
+        /*
+            The next parameters define the size of the render area. The render area defines where shader loads and stores will take place. 
+            The pixels outside this region will have undefined values. It should match the size of the attachments for best performance.
+        */
+        renderPassInfo.renderArea.offset = { 0, 0 };
+        renderPassInfo.renderArea.extent = swapChainExtent;
+
+        VkClearValue clearColor = { {{0.0f, 0.0f, 0.0f, 1.0f}} };
+        /*
+            The last two parameters define the clear values to use for VK_ATTACHMENT_LOAD_OP_CLEAR, which we used as load 
+            operation for the color attachment. We set the clear color to simply be black with 100% opacity.
+        */
+        renderPassInfo.clearValueCount = 1;
+        renderPassInfo.pClearValues = &clearColor;
+
+        /*
+            All of the functions that record commands can be recognized by their vkCmd prefix. 
+            They all return void, so there will be no error handling until we’ve finished recording.
+            The first parameter for every command is always the command buffer to record the command to. 
+            The second parameter specifies the details of the render pass.. 
+            The final parameter controls how the drawing commands within the render pass will be provided. It can have one of two values:
+
+            VK_SUBPASS_CONTENTS_INLINE: The render pass commands will be embedded in the primary command buffer itself and no secondary command buffers will be executed.
+
+            VK_SUBPASS_CONTENTS_SECONDARY_COMMAND_BUFFERS: The render pass commands will be executed from secondary command buffers.
+
+            We will not be using secondary command buffers, so we will go with the first option.
+        */
+        vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+        /*
+            Binds the graphics pipeline. The second parameter specifies if the pipeline object is a graphics or compute pipeline. 
+            We’ve now told Vulkan which operations to execute in the graphics pipeline and which attachment to use in the fragment shader.
+        */
+        vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
+
+        /*
+            In the createGraphicsPipeline method we specified viewport and scissor state to be dynamic. 
+            Therefore, we need to set them in the command buffer before issuing our draw command.
+        */
+        VkViewport viewport {};
+
+        viewport.x = 0.0f;
+        viewport.y = 0.0f;
+        viewport.width = static_cast<float>(swapChainExtent.width);
+        viewport.height = static_cast<float>(swapChainExtent.height);
+        viewport.minDepth = 0.0f;
+        viewport.maxDepth = 1.0f;
+
+        vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+        
+        /*
+            While viewports define the transformation from the image to the framebuffer, scissor rectangles define in
+            which regions pixels will actually be stored. Any pixels outside the scissor rectangles will be discarded
+            by the rasterizer. They function like a filter rather than a transformation.
+            If we want to draw to the entire framebuffer, specify a scissor rectangle that covers it entirely.
+        */
+        VkRect2D scissor {};
+
+        scissor.offset = { 0, 0 };
+        scissor.extent = swapChainExtent;
+        
+        vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
+
+        /*
+            Now we are ready to issue the draw command for the triangle.
+        
+            vertexCount: Even though we don’t have a vertex buffer, we technically still have 3 vertices to draw.
+
+            instanceCount: Used for instanced rendering, use 1 if you’re not doing that.
+
+            firstVertex: Used as an offset into the vertex buffer, defines the lowest value of gl_VertexIndex.
+
+            firstInstance: Used as an offset for instanced rendering, defines the lowest value of gl_InstanceIndex.
+        */
+        vkCmdDraw(commandBuffer, 3, 1, 0, 0);
+
+        vkCmdEndRenderPass(commandBuffer);
+
+        if (vkEndCommandBuffer(commandBuffer) != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed to record command buffer.");
+        }
+    }
+
     void initVulkan() 
     {
         createInstance();
@@ -1577,48 +1773,45 @@ private:
         createRenderPass();
         createGraphicsPipeline();
         createFramebuffers();
+        createCommandPool();
+        createCommandBuffer();
     }
 
-    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) 
+    /*
+        Rendering a frame in Vulkan consists of a common set of steps:
+
+        1. Wait for the previous frame to finish
+
+        2. Acquire an image from the swap chain
+
+        3. Record a command buffer which draws the scene onto that image
+
+        4. Submit (execute) the recorded command buffer
+
+        5. Return the finished image to the swap chain and present it on the surface
+    */
+    void drawFrame()
     {
-        createInfo = {};
-        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-        createInfo.pfnUserCallback = debugCallback; // pointer to the callback function
-        createInfo.pUserData = nullptr; // Optional, such as a pointer to the HelloTriangleApplication class
+        
     }
 
-    void setupDebugMessenger()
-    {
-        if (!enableValidationLayers) return;
-
-        // Fill in a struct with details about the messenger and its callback.
-        VkDebugUtilsMessengerCreateInfoEXT createInfo;
-
-        populateDebugMessengerCreateInfo(createInfo);
-
-        /*
-            Since the debug messenger is specific to our Vulkan instance and its layers, it needs to be explicitly specified as first argument. 
-            You will also see this pattern with other child objects later on.
-        */
-        if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) 
-        {
-            throw std::runtime_error("Failed to set up debug messenger.");
-        }
-    }
+    
 
     void mainLoop() 
     {
         while (!glfwWindowShouldClose(window))
         {
             glfwPollEvents();
+
+            drawFrame();
         }
     }
 
     // Note the reverse order of deletions based on dependencies
     void cleanup() 
     {
+        vkDestroyCommandPool(device, commandPool, nullptr);
+
         for (auto framebuffer : swapChainFramebuffers)
         {
             vkDestroyFramebuffer(device, framebuffer, nullptr);
